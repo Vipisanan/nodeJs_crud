ad85a401880dd3c4c071da9280f6fdc395b1dada
vipisanan <vipisanan1@gmail.com>
2020-09-27 23:37:53 +0530

    get methodes call

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const app =express()
 mongoose.connect(url , {useNewUrlParser: true})
 const con = mongoose.connection
 
-con.on('open' , function(){
+con.on('open' , ()=>{
     console.log("connected....");
+})
+const aliensRouter = require('./routers/aliens')
+app.use('/aliens',aliensRouter)
+app.listen(9000 , ()=>{
+    console.log("Server started")
 })
